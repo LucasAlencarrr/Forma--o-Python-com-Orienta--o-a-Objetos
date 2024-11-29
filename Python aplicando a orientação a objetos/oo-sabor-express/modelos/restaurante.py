a5993260cee8b836234399a919ec3ff994d7598d
Lucas Alencar <lucasalencar.br@gmail.com>
2024-11-29 00:03:34 -0400

    Acrescentando alguns arquivos

--- Python aplicando a orientação a objetos/oo-sabor-express/modelos/restaurante.py
+++ Python aplicando a orientação a objetos/oo-sabor-express/modelos/restaurante.py
@@ -2,21 +2,23 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria}| {restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
-restaurante_praca = Restaurante(nome = 'Praça',categoria = 'Gourmet')
-restaurante_pizza = Restaurante(nome = 'Pizza Express',categoria = 'Italiana')
-
-Restaurante.listar_restaurantes()
-
-
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
